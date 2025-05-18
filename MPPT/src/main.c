d58a88e4a2c18c7e2cf63e15388b47676fef880e
danimauriz <dani_mauriz@hotmail.com>
2025-05-18 13:23:49 +0200

    Añado lectura de entradas analógicas. Debo corregir que recoja bien la lectura real del voltaje que hay en la patilla. WS de momento aún no funciona. Actualiza valor de los sensores refrescando la web.

--- MPPT/src/main.c
+++ MPPT/src/main.c
@@ -10,16 +10,35 @@
 #include "esp_http_server.h"
 #include "esp_log.h"
 #include "esp_random.h"
+#include "esp_system.h"
+#include "esp_spiffs.h"
+#include "esp_adc/adc_oneshot.h"
+#include "hal/adc_types.h"
+
+#include "esp_adc/adc_cali_scheme.h"
 
 
-#define PWM_FREQ       5000
+
+#define PWM_FREQ       2000
 #define PWM_CHANNEL    LEDC_CHANNEL_0
 #define PWM_TIMER      LEDC_TIMER_0
-#define PWM_GPIO       23
+#define PWM_GPIO       25
+
+static const char *TAG = "WebSocket Server"; // TAG for debug
+
+httpd_handle_t server = NULL;
+
+adc_oneshot_unit_handle_t adc_handle = NULL;
+
+struct async_resp_arg {
+    httpd_handle_t hd;
+    int fd;
+};
 
 // Variables compartidas
-float sensor_value = 0;
-float algorithm_result = 0;
+volatile float current = 0;
+volatile float panel_voltage = 0.0;
+volatile float battery_voltage = 0.0;
 int duty_cycle = 0;
 
 // Mutex para sincronización
@@ -47,20 +66,92 @@ void init_pwm() {
     ledc_channel_config(&channel_conf);
 }
 
+void config_ADC() {
+
+    adc_oneshot_unit_init_cfg_t init_config1 = {
+    .unit_id = ADC_UNIT_1,
+    .ulp_mode = ADC_ULP_MODE_DISABLE,
+    };
+
+    ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config1, &adc_handle));
+
+    adc_oneshot_chan_cfg_t config = {
+    .bitwidth = ADC_BITWIDTH_12, // or you can use ADC_BITWIDTH_DEFAULT here
+    .atten = ADC_ATTEN_DB_12,
+    };
+
+    ESP_ERROR_CHECK(adc_oneshot_config_channel(adc_handle, ADC_CHANNEL_6, &config));
+}
+
+
+float read_current() {
+    int adc_current_sense = 0;
+    esp_err_t err = adc_oneshot_read(adc_handle, ADC_CHANNEL_6, &adc_current_sense);
+    if (err != ESP_OK) {
+        printf("ACD current sensor read failed: %s", esp_err_to_name(err));
+    }
+    printf(" ADC_CHANNEL_6 (GPIO 34) ADC input current _sensor:   %d \n", adc_current_sense);
+
+
+
+    return (float)adc_current_sense;
+}
+
+float read_panel_voltage() {
+    int adc_panel_voltage_sense = 0;
+    float voltage = 0.0;
+    esp_err_t err = adc_oneshot_read(adc_handle, ADC_CHANNEL_7, &adc_panel_voltage_sense);
+    if (err != ESP_OK) {
+        printf("ACD panel voltage read failed: %s", esp_err_to_name(err));
+    }
+    printf(" ADC_CHANNEL_7 (GPIO 35) ADC input panel voltage:   %d \n", adc_panel_voltage_sense);
+    voltage = adc_panel_voltage_sense * 45 / 4096;
+    printf(" Input panel voltage:   %.2f \n", voltage);
+
+    return voltage;
+}
+
+float read_battery_voltage() {
+    int adc_battery_voltage_sense = 0;
+    float voltage = 0.0;
+    esp_err_t err = adc_oneshot_read(adc_handle, ADC_CHANNEL_4, &adc_battery_voltage_sense);
+    if (err != ESP_OK) {
+        printf("ACD battery voltage read failed: %s", esp_err_to_name(err));
+    }
+    printf(" ADC_CHANNEL_4 (GPIO 32) ADC input battery voltage:   %d \n", adc_battery_voltage_sense);
+    voltage = adc_battery_voltage_sense * 16.5 / 4096;
+    printf(" Input battery voltage:   %.2f \n", voltage);
+
+    return voltage;
+}
+
 // --- Tarea en núcleo 0: Lee sensores y ajusta PWM ---
 void sensor_task(void *pvParameters) {
     while (1) {
+        /*
         float read = esp_random() % 1000 / 10.0f; // Simulación sensor
         float result = read * 1.5f;               // Simulación algoritmo
 
         int duty = (int)(result * 10);
         if (duty > 1023) duty = 1023;
+        */
+        
+
+        float current_sense = read_current();
+        float panel_voltage_sense = read_panel_voltage();
+        float battery_voltage_sense = read_battery_voltage();
 
         taskENTER_CRITICAL(&data_mux);
-        sensor_value = read;
-        algorithm_result = result;
-        duty_cycle = duty;
+        current = current_sense;
+        panel_voltage = panel_voltage_sense;
+        battery_voltage = battery_voltage_sense;
         taskEXIT_CRITICAL(&data_mux);
+
+        int duty = 1023*0.33;
+
+        printf("Duty: %i\n", duty);
+
+         
 
         ledc_set_duty(LEDC_HIGH_SPEED_MODE, PWM_CHANNEL, duty);
         ledc_update_duty(LEDC_HIGH_SPEED_MODE, PWM_CHANNEL);
@@ -69,22 +160,116 @@ void sensor_task(void *pvParameters) {
     }
 }
 
+static void ws_async_send(void *arg)
+{
+    httpd_ws_frame_t ws_pkt;
+    struct async_resp_arg *resp_arg = arg;
+    httpd_handle_t hd = resp_arg->hd;
+    int fd = resp_arg->fd;
+    
+    char buff[32];
+    memset(buff, 0, sizeof(buff));
+    sprintf(buff, "%f",current);
+    
+    memset(&ws_pkt, 0, sizeof(httpd_ws_frame_t));
+    ws_pkt.payload = (uint8_t *)buff;
+    ws_pkt.len = strlen(buff);
+    ws_pkt.type = HTTPD_WS_TYPE_TEXT;
+    
+    static size_t max_clients = CONFIG_LWIP_MAX_LISTENING_TCP;
+    size_t fds = max_clients;
+    int client_fds[max_clients];
+
+    esp_err_t ret = httpd_get_client_list(server, &fds, client_fds);
+
+    if (ret != ESP_OK) {
+        return;
+    }
+
+    for (int i = 0; i < fds; i++) {
+        int client_info = httpd_ws_get_fd_info(server, client_fds[i]);
+        if (client_info == HTTPD_WS_CLIENT_WEBSOCKET) {
+            httpd_ws_send_frame_async(hd, client_fds[i], &ws_pkt);
+        }
+    }
+    free(resp_arg);
+}
+
+static esp_err_t trigger_async_send(httpd_handle_t handle, httpd_req_t *req)
+{
+    struct async_resp_arg *resp_arg = malloc(sizeof(struct async_resp_arg));
+    resp_arg->hd = req->handle;
+    resp_arg->fd = httpd_req_to_sockfd(req);
+    return httpd_queue_work(handle, ws_async_send, resp_arg);
+}
+
 // --- Web server handler ---
 esp_err_t web_handler(httpd_req_t *req) {
     char resp[256];
 
     taskENTER_CRITICAL(&data_mux);
-    float s = sensor_value;
-    float a = algorithm_result;
+    float c = current;
+    float p = panel_voltage;
+    float b = battery_voltage;
     int d = duty_cycle;
     taskEXIT_CRITICAL(&data_mux);
 
     snprintf(resp, sizeof(resp),
              "<html><body><h1>ESP32 Monitor</h1>"
-             "Sensor: %.2f<br>Algoritmo: %.2f<br>Duty Cycle: %d<br>"
-             "</body></html>", s, a, d);
+             "Current_Sensor: %.2f<br>Panel_Voltage: %.2f<br>Battery_Voltage: %.2f<br>Duty Cycle: %d<br>"
+             "</body></html>", c, p, b, d);
 
     httpd_resp_send(req, resp, HTTPD_RESP_USE_STRLEN);
+    return ESP_OK;
+}
+
+static esp_err_t handle_ws_req(httpd_req_t *req)
+{
+    if (req->method == HTTP_GET)
+    {
+        ESP_LOGI(TAG, "Handshake done, the new connection was opened");
+        return ESP_OK;
+    }
+
+    httpd_ws_frame_t ws_pkt;
+    uint8_t *buf = NULL;
+    memset(&ws_pkt, 0, sizeof(httpd_ws_frame_t));
+    ws_pkt.type = HTTPD_WS_TYPE_TEXT;
+    esp_err_t ret = httpd_ws_recv_frame(req, &ws_pkt, 0);
+    if (ret != ESP_OK)
+    {
+        ESP_LOGE(TAG, "httpd_ws_recv_frame failed to get frame len with %d", ret);
+        return ret;
+    }
+
+    if (ws_pkt.len)
+    {
+        buf = calloc(1, ws_pkt.len + 1);
+        if (buf == NULL)
+        {
+            ESP_LOGE(TAG, "Failed to calloc memory for buf");
+            return ESP_ERR_NO_MEM;
+        }
+        ws_pkt.payload = buf;
+        ret = httpd_ws_recv_frame(req, &ws_pkt, ws_pkt.len);
+        if (ret != ESP_OK)
+        {
+            ESP_LOGE(TAG, "httpd_ws_recv_frame failed with %d", ret);
+            free(buf);
+            return ret;
+        }
+        ESP_LOGI(TAG, "Got packet with message: %s", ws_pkt.payload);
+        printf("Got packet with message: %s", ws_pkt.payload);
+    }
+
+    ESP_LOGI(TAG, "frame len is %d", ws_pkt.len);
+
+    if (ws_pkt.type == HTTPD_WS_TYPE_TEXT &&
+        strcmp((char *)ws_pkt.payload, "toggle") == 0)
+    {
+        free(buf);
+        return trigger_async_send(req->handle, req);
+    }
     return ESP_OK;
 }
 
@@ -93,17 +278,27 @@ void start_webserver() {
     httpd_config_t config = HTTPD_DEFAULT_CONFIG();
     httpd_handle_t server = NULL;
 
-    httpd_start(&server, &config);
-
     httpd_uri_t uri = {
         .uri = "/",
         .method = HTTP_GET,
         .handler = web_handler,
         .user_ctx = NULL
     };
-    httpd_register_uri_handler(server, &uri);
-}
 
+    httpd_uri_t ws = {
+        .uri = "/ws",
+        .method = HTTP_GET,
+        .handler = handle_ws_req,
+        .user_ctx = NULL,
+        .is_websocket = true
+    };
+
+    if (httpd_start(&server, &config) == ESP_OK)
+    {
+        httpd_register_uri_handler(server, &uri);
+        httpd_register_uri_handler(server, &ws);
+    }
+}
 // --- Init WiFi en modo AP ---
 void init_wifi_ap() {
     esp_netif_init();
@@ -140,6 +335,7 @@ void web_task(void *pvParameters) {
 void app_main(void) {
     nvs_flash_init(); // Necesario para WiFi
     init_pwm();
+    config_ADC();
 
     xTaskCreatePinnedToCore(sensor_task, "SensorTask", 4096, NULL, 1, NULL, 0);
     xTaskCreatePinnedToCore(web_task,    "WebTask",    4096, NULL, 1, NULL, 1);
